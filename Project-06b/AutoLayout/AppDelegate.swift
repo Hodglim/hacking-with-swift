9292f1720dd63bd0357ef15b67b463dc455f5622
Hodglim <github@hodglim.com>
2015-10-22 12:13:58 +0100

    Project 06b - Initial commit.

---
+++ Project-06b/AutoLayout/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  AutoLayout
+//
+//  Created by Darren Hodges on 13/10/2015.
+//  Copyright © 2015 Darren Hodges. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+}
---
+++ Project-06b/README.md
@@ -0,0 +1,4 @@
+# Project-06b: Auto Layout in code
+My project source code from following Paul Hudson's superb Hacking With Swift tutorial series. https://www.hackingwithswift.com
+
+Get to grips with Auto Layout in code.
