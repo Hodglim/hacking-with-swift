75cbc581a945c6b37236942535e5014984d01a95
Darren Hodges <dhodges@addictiveinteractive.com>
2016-06-24 15:28:06 +0100

    Project 18 - Initial commit.

---
+++ Project-18/README.md
@@ -0,0 +1,5 @@
+# Project-18: iAds
+My project source code from following Paul Hudson's superb Hacking With Swift tutorial series. https://www.hackingwithswift.com
+
+How to place adverts.
+Includes: IAd
---
+++ Project-18/iAds/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  iAds
+//
+//  Created by Darren Hodges on 24/06/2016.
+//  Copyright © 2016 Darren Hodges. All rights reserved.
+//
+
+import UIKit
+import iAd
+
+class ViewController: UIViewController, ADBannerViewDelegate
+{
+	var bannerView: ADBannerView!
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		
+		bannerView = ADBannerView(adType: .Banner)
+		bannerView.translatesAutoresizingMaskIntoConstraints = false
+		bannerView.delegate = self
+		bannerView.hidden = true
+		view.addSubview(bannerView)
+		
+		let viewsDictionary = ["bannerView": bannerView]
+		view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+		view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bannerView]|", options: [], metrics: nil, views: viewsDictionary))
+	}
+
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func bannerViewDidLoadAd(banner: ADBannerView!)
+	{
+		bannerView.hidden = false
+	}
+	
+	func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!)
+	{
+		bannerView.hidden = true
+	}
+}
+
